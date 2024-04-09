c573f932a8486da6399663d30c663a12cbb4aa4e
Teo <teccent@yandex.ru>
2024-04-09 14:19:48 +0300

    Initial Commit

---
+++ SliderGameApp/SliderGameAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameAppApp.swift
+//  SliderGameApp
+//
+//  Created by Теона Магай on 09.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
